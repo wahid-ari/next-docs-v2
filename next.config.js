958da5fe6e340834e85a9291d4bd08d955ef8090
wahid-ari <wahiid.arii@Gmail.com>
2022-10-27 14:33:45 +0700

    update

--- next.config.js
+++ next.config.js
@@ -6,3 +6,43 @@ const withNextra = require('nextra')({
 })
 
 module.exports = withNextra({})
+
+// const { withSentryConfig } = require("@sentry/nextjs");
+// const withNextra = require("nextra")({
+//   theme: "nextra-theme-docs",
+//   themeConfig: "./theme.config.js",
+//   unstable_flexsearch: true,
+//   unstable_staticImage: true,
+// });
+
+// const sentryWebpackPluginOptions = {
+//   silent: true,
+// };
+
+// const OLD_TURBOREPO_ROUTES = [
+//   "/docs",
+//   "/docs/ci/circleci",
+// ];
+
+// const nextConfig = withNextra({
+//   sentry: {
+//     hideSourceMaps: true,
+//   },
+//   reactStrictMode: true,
+//   experimental: {
+//     legacyBrowsers: false,
+//   },
+//   webpack: (config, { webpack }) => {
+//     config.plugins.push(
+//       new webpack.DefinePlugin({
+//         __SENTRY_DEBUG__: false,
+//         __SENTRY_TRACING__: false,
+//       })
+//     );
+
+//     // return the modified config
+//     return config;
+//   },
+// });
+
+// module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
